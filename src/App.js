0fc4558ed57bf64c0f8260a51db666745dbd245c
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-07-20 23:11:45 +0300

    Solution for exercise 1.7

--- src/App.js
+++ src/App.js
@@ -35,12 +35,18 @@ const Button = ({ text, handleClick }) => {
 }
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = ((good * 1) + (bad * -1)) / all
+  const positive = (good / all) * 100
   return (
     <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
